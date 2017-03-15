79d0c0708074cfcf749c8cace9c4f9047dd7db5f
Iason Dimitrakopoulos <idimitrakopoulos@gmail.com>
2017-03-15 09:05:46 +0200

    changes to conform with general hardware

--- hw/screen/Screen.py
+++ hw/screen/Screen.py
@@ -1,18 +1,26 @@
-from lib.toolkit import find_enum_id
+class ScreenType:
+    SSD1306 = 0
+    SSD1106 = 1
+
+    def __init__(self):
+        pass
+
+class ConnectionType:
+    I2C = 0
+    SPI = 1
+
+    def __init__(self):
+        pass
 
 
 class Screen:
-    type_mapping = ""
-    connection_type_mapping = ""
     type = ""
     connection_type = ""
 
 
     # @timed_function
-    def __init__(self, type, connection_type, pins):
-        self.type_mapping = list(enumerate(['OLED']))
-        self.connection_type_mapping = list(enumerate(['I2C', 'SPI']))
-        self.type = find_enum_id(self.type_mapping, type)
-        self.connection_type = find_enum_id(self.connection_type_mapping, connection_type)
+    def __init__(self, screen_type, connection_type, pins):
+        self.type = screen_type
+        self.connection_type = connection_type
 
 
