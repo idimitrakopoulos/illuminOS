ab14c911a7427ce807b20a549b75515a79b08004
idimitrakopoulos <idimitrakopoulos@gmail.com>
2017-05-05 10:47:18 +0300

    added profiles

--- profile/plant_monitor/main.py
+++ profile/plant_monitor/main.py
@@ -1,8 +1,8 @@
 from lib.toolkit import log, load_properties
-profile = load_properties("conf/profile.properties")
+props = load_properties("conf/profile.properties")
 
 # 364 is submerged in water and 1024 is dry
 from hw.sensor.AnalogSensor import AnalogSensor
 
-soil_sensor = AnalogSensor(0, 1024, 364)
+soil_sensor = AnalogSensor(int(props["analog_pin"]), int(props["vhigh"]), int(props["vlow"]))
 soil_sensor.get_value()
